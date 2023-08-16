9db3cf91d9242f458593ba4b32a160f3afecadfb
Enoch <enochmokny@gmail.com>
2023-08-16 11:49:20 +1000

    enabled algo1

--- main_CMMD.py
+++ main_CMMD.py
@@ -4,6 +4,9 @@ import torch
 import pandas as pd
 import os
 from data_loading.data_loader import createDataLoaders
+from data_preprocess.dicom_conversion import load_and_preprocess_dicom
+from data_preprocess.normalize_intensity import normalize_intensity
+from data_preprocess.resample import resample_to_resolution
 from train.train_loader import train
 from models.create_model import createModel
 from performance.show_graph import plotGraph
@@ -20,9 +23,9 @@ train_images = []
 val_images = []
 train_labels =[]
 val_labels =[]
-data_folder = '/home/emok/sq58_scratch/emok/Data/CMMD/CMMD/'
-dataset_name = data_folder.split("/")[-2]
-data_folder = os.path.join(data_folder, 'dataset/CMMD')
+home_dir = '/home/emok/sq58_scratch/emok/Data/CMMD/'
+dataset_name = home_dir.split("/")[-2]
+data_folder = os.path.join(home_dir, 'CMMD')
 RESAMPLE_RESOLUTION = (224, 224)
 TRAIN_RATIO = 0.7  # 70% for training
 TEST_RATIO = 0.2   # 20% for testing
@@ -34,49 +37,49 @@ print(f"Device: {device}")
 
 
 
-dataframe_directory = os.path.join(data_folder, 'cleaned_clinicaldata.csv')
+dataframe_directory = os.path.join(home_dir, 'cleaned_clinicaldata.csv')
 df = pd.read_csv(dataframe_directory)
 
 # Algo 1 (run this if first time)
 
-# for root, dirs, files in os.walk(data_folder):
-#         for file in files:
-#             if file.lower().endswith('.dcm'):
-#                 curr_file_path = os.path.join(root, file)
-#                 pixel_data, dicom_data = load_and_preprocess_dicom(curr_file_path)
-#                 normalized_data = normalize_intensity(pixel_data)
-#                 resampled_image = resample_to_resolution(normalized_data, dicom_data, RESAMPLE_RESOLUTION)
-#                 all_images.append(resampled_image)
+for root, dirs, files in os.walk(data_folder):
+        for file in files:
+            if file.lower().endswith('.dcm'):
+                curr_file_path = os.path.join(root, file)
+                pixel_data, dicom_data = load_and_preprocess_dicom(curr_file_path)
+                normalized_data = normalize_intensity(pixel_data)
+                resampled_image = resample_to_resolution(normalized_data, dicom_data, RESAMPLE_RESOLUTION)
+                all_images.append(resampled_image)
 
 
-#                 # Find corresponding label
-#                 # uid_instance_id = root.split("/")[-1]
-#                 # image_label = df.loc[df['uid_instance_id'] == uid_instance_id, 'pathology']
-#                 # print(f"uid_instance_id: {uid_instance_id}")
-#                 # print(f"image_label: {image_label}")
+                # Find corresponding label
+                # uid_instance_id = root.split("/")[-1]
+                # image_label = df.loc[df['uid_instance_id'] == uid_instance_id, 'pathology']
+                # print(f"uid_instance_id: {uid_instance_id}")
+                # print(f"image_label: {image_label}")
 
-#                 # exists = df['uid_instance_id'].isin([uid_instance_id]).any()
+                # exists = df['uid_instance_id'].isin([uid_instance_id]).any()
 
-#                 # if exists:
-#                 #     print(f"{uid_instance_id} exists in the column.")
-#                 # else:
-#                 #     print(f"{uid_instance_id} does not exist in the column.")
+                # if exists:
+                #     print(f"{uid_instance_id} exists in the column.")
+                # else:
+                #     print(f"{uid_instance_id} does not exist in the column.")
 
-#                 # Find corresponding label (CMMD)
-#                 ID1 = root.split("/")[-3]
-#                 # print(ID1)
-#                 # exists = df['ID1'].isin([ID1]).any()
+                # Find corresponding label (CMMD)
+                ID1 = root.split("/")[-3]
+                # print(ID1)
+                # exists = df['ID1'].isin([ID1]).any()
 
-#                 # if exists:
-#                 #     print(f"{ID1} exists in the column.")
-#                 # else:
-#                 #     print(f"{ID1} does not exist in the column.")
+                # if exists:
+                #     print(f"{ID1} exists in the column.")
+                # else:
+                #     print(f"{ID1} does not exist in the column.")
 
-#                 index = df.index[df['ID1'] == ID1].tolist()[0]
+                index = df.index[df['ID1'] == ID1].tolist()[0]
 
-#                 # Get the corresponding value from ColumnB
-#                 image_label = df.at[index, 'class_label']
-#                 all_labels.append(image_label)
+                # Get the corresponding value from ColumnB
+                image_label = df.at[index, 'class_label']
+                all_labels.append(image_label)
 
 
 # # Algo 3 (Pickle) (use this for quicker retrieval of data after Algo 1 was done before)
